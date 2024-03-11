dcfe59fda850d51994a86e60480b6de655fd4d61
Olga <jilliya13@gmmail.com>
2024-03-11 20:48:59 +0300

    add seminar2:task2

---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и возводит вторую цифру этого числа в степень, равную третьей цифре.
+// Примеры
+// 487 => 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+Console.WriteLine("Введите целое трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int secondDigit = number / 10 % 10;
+    int numDegree = number % 10;
+    int result = 1;
+    int count = 0;
+    while (count < numDegree)
+    {
+        result *= secondDigit; // result = result * secondDigit
+        count++;
+    }
+    Console.WriteLine(result);
+}
