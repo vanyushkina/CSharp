fa0a2bca7b83491eed9fa1e695a702e61430ff43
Olga <jilliya13@gmmail.com>
2024-03-11 20:35:45 +0300

    add seminar2:task1

--- Seminar2/Task1/Program.cs
+++ Seminar2/Task1/Program.cs
@@ -6,8 +6,15 @@
 Console.WriteLine("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int firstDigit = number / 100; // 256 / 100 = 2.56 => 2
-int thirdDigit = number % 10; // 256 % 10 = 250 + 6 => 6
-int result = firstDigit * 10 + thirdDigit;
+if (number >= 100 && number <= 999 || number >= -999 && number <= -100)
+{
+    int firstDigit = number / 100; // 256 / 100 = 2.56 => 2
+    int thirdDigit = number % 10; // 256 % 10 = 250 + 6 => 6
+    int result = firstDigit * 10 + thirdDigit;
 
-Console.WriteLine(" => " + result);
+    Console.WriteLine(" => " + result);
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
