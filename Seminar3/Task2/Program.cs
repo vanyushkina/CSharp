a879571c562f30b24a40f7d738b9dd252f349bf1
Olga <jilliya13@gmmail.com>
2024-03-14 21:12:12 +0300

    seminar3:task2

--- Seminar3/Task2/Program.cs
+++ Seminar3/Task2/Program.cs
@@ -9,6 +9,13 @@ int[] array = new int [10] {-10, -7, -5, -4, -1, 0, 4, 5, 7, 10};
 int length = array.Length;
 int index = 0;
 
+for (int i = 0; i < length; i++) // Печатаем массив с ',' и последний элемент без ','
+     {   if (i < length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write(array[i]);
+     }
+
 while(index < length)
 {
     //array[index] = array[index]* -1;
