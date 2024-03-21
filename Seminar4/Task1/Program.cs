fbe97a9cd71fd8787cdec42432c80bda61b5f969
Olga <jilliya13@gmmail.com>
2024-03-21 20:37:08 +0300

    add seminar4:task1

---
+++ Seminar4/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Определите количество простых чисел в этом массиве.
+//Примеры:
+//[1 3 4 19 3] => 2
+//[4 3 4 1 9 5 21 13] =>3
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRndInt(5, 1, 100);
+PrintArray(arr);
