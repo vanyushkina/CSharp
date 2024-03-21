12128cb184ff5cfd1b2f4855ce96ee913139ee9e
Olga <jilliya13@gmmail.com>
2024-03-21 21:19:47 +0300

    add seminar4:task2

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задайте массив из N случайных целых чисел (N вводится с клавиатуры).
+//Найдите количество чисел, которые оканчиваютс на 1 и делтсяя нацело на 7.
+//Пример:
+//[1 5 11 21 81 4 0 91 2 3] => 2
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int NumberCountCheck(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 1 == 1 && arr[i] % 7 == 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateArrayRndInt(n, 1, 100);
+PrintArray(newArray);
+Console.WriteLine();
+Console.WriteLine(NumberCountCheck(newArray));
