6897ac3736a00f4eb2579b34d35374c26274a64c
Olga <jilliya13@gmmail.com>
2024-03-23 21:11:17 +0300

    add lesson4:task3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLettersFromStr(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromStr(str);
+Console.WriteLine(result);
