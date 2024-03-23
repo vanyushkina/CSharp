97133300b78e0f0fb7877bd6b70dcb49099055b0
Olga <jilliya13@gmmail.com>
2024-03-23 20:14:14 +0300

    lesson4:task2

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿int[,] CreateMatrix (int rowsCount, int columnsCount)
+{
+    int[,] matrix = new int [rowsCount, columnsCount];
+    
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} "); //интерполяция строк
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4);
+
+foreach (int e in matrix)
+{
+    if (IsInteresting(e) == true)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+bool IsInteresting(int value)
+{
+    int sumOfDigits = GetSumOfDigits(value);
+    if (sumOfDigits % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum += value % 10;
+        value /= 10;
+    }
+    return sum;
+}
+
+ShowMatrix(matrix);
+Console.WriteLine();
