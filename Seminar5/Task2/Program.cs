c561b3815f8099c3bcfc9d810dbbe80ec7b4eb9f
Olga <jilliya13@gmmail.com>
2024-03-25 19:44:52 +0300

    add seminar5:task3

---
+++ Seminar5/Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1,1) и т.д.).
+//Пример:
+//2 3 4 3
+//4 3 4 1   => 2 + 3 + 5 = 10
+//2 9 5 4
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        //Console.WriteLine("]");
+        Console.WriteLine();
+    }
+}
+
+int SumElemMainDiagonal(int[,] matrix)
+{
+    int result = 0;
+    //int size = matrix.GetLength(0);
+    //if(size > matrix.GetLength(1)) size = matrix.GetLength(1);
+
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        result = result + matrix[i, i];
+    }
+    return result;
+}
+
+int[,] matr = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(matr);
+int SumMainDiagonal = SumElemMainDiagonal(matr);
+Console.WriteLine(SumMainDiagonal);
---
+++ Seminar5/Task3/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задайте двумерный массив из целыхх чисел.
+//Сформируйте новый одномерный массив, состоящий из средних арифметических значений по строкам двумерного массива.
+// Пример:
+//2 3 4 3 
+//4 3 4 1   => [3 3 5]
+//2 9 5 4 
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        //Console.WriteLine("]");
+        Console.WriteLine();
+    }
+}
+
+double[] ArraySumAverage(int[,] matrix)
+{
+    double[] ArrayResult = new double[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int SumRow = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            SumRow = SumRow + matrix[i, j];
+        }
+        ArrayResult[i] = (double) SumRow / matrix.GetLength(1);
+    }
+}
+return ArrayResult;
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[,] matr = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(matr);
+Console.WriteLine();
+double[] array = ArraySumAverage(matr);
+PrintArray(array);
