734deb8371dfdcfdacad3ae6a46a454a96ff84d6
Olga <jilliya13@gmmail.com>
2024-03-25 20:27:14 +0300

    add seminar6:task1

---
+++ Seminar6/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте массив символов (тип char[]). Создайте строку из символов этого массива.
+//Пример
+//[‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
+
+
+static string ArrayCharsToString(char[] chars, string str)
+{
+    for (int i = 0; i < chars.Length; i++)
+    {
+        str += chars[i]; // str = str + chars[i]
+    }
+
+    return str;
+}
+
+char[] chars = {'a', 'b', 'c', 'd'};
+string str = string.Empty;
+
+str = ArrayCharsToString(chars, str);
+Console.WriteLine(str);
+
