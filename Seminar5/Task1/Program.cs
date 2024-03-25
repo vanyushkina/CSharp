7faa8f233283496bad9f4deda51fb530fb0203e5
Olga <jilliya13@gmmail.com>
2024-03-25 18:32:26 +0300

    add seminar5:task1

---
+++ Seminar5/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+//Пример:
+//2 3 4 3           4 3 16 3
+//4 3 4 1    =>     4 3 4 1
+//2 9 5 4           4 9 25 4
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        //Console.WriteLine("]");
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
