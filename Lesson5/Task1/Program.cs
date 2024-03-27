e41765955170a4792300b3e634f5969644229de7
Olga <jilliya13@gmmail.com>
2024-03-27 21:00:21 +0300

    add lesson5:task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int Fact(int n)
+{
+    if(n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+Console.Write(Fact(5));
