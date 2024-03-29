8c341106f264dfdf2a3d731fefc093fd787e99e9
Olga <jilliya13@gmmail.com>
2024-03-29 21:29:45 +0300

    add seminar7:task2

---
+++ Seminar7/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//Пример:
+// 1 2 3 => 6
+// 6 3 => 9
+
+
+int SumOfDigitsNum(int num)
+{
+    if(num == 0) return 0;
+    return num % 10 + SumOfDigitsNum(num / 10);
+}
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumOfDigitsNum(number));
