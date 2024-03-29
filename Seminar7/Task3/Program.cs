83dc67fce8cad368d23ff667f6e96e46f58c012e
Olga <jilliya13@gmmail.com>
2024-03-29 21:54:02 +0300

    add seminar7:task3

---
+++ Seminar7/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Считать стоку с консоли, содержащую латинские буквы. Вывести на экран согласные буквы этой строки.
+//Пример:
+//"hello" => h l l
+//"world" => w r l d
+//"Hello world!" => H l l w r l d
+
+
+void PrintOnlyConsonans(string line)
+{
+    if(line.Length == 0) return;
+    string chars = "aeiouy";
+        if(char.IsAsciiLetter(line[0]) && !chars.Contains(line[0]))
+            Console.Write($"{line[0]}");
+        PrintOnlyConsonans(line.Substring(1));
+}
+
+
+Console.WriteLine("Введите строку: ");
+string str = Console.ReadLine();
+PrintOnlyConsonans(str);
